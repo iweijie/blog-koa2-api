9fab6259a3b8f008719e72e71ffea7e6c28887eb
weijie <375030086@qq.com>
2018-09-16 22:40:13 +0800

    新增etag依赖

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const config = require("./config/index")
 const verifyLogin = require("./app/middleware/verify_login")
 const error = require("./app/middleware/error")
 const router = require("./app/middleware/router")
+const etag = require('koa-etag')();
 require('./app/models/index')
 
 const app = new Koa()
@@ -30,15 +31,25 @@ app.use(cors({
 	allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
 }));
 // 参数解析
-app.use(koabody({ multipart: true}))
+app.use(koabody({ multipart: true }))
 
 // 验证是否登入
 app.use(verifyLogin)
+
+app.use(etag)
 // 引入路由
 router(app)
 
 app.use(async (ctx) => {
-	ctx.body = {a:1,B:2};
+	ctx.body = { state: 2, msg: "Not Find" };
 })
 
 app.listen(config.port)
+
+process.on('unhandledRejection', (err) => {
+	// logger.fatal(`unhandledRejection: ${err.message}, stack: ${err.stack}`);
+});
+
+process.on('uncaughtException', (err) => {
+	// logger.fatal(`uncaughtException: ${err.message}, stack: ${err.stack}`);
+});

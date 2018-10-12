4475dd1fcce4261b0098aebd6a4332b480ec8960
weijie <375030086@qq.com>
2018-10-12 11:47:04 +0800

    修改pm2 启动配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,26 +4,21 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
   apps : [
-
     // First application
     {
       name      : 'blogApi',
-      script    : './app.js',
+      script    : './app',
+      watch     :  true ,
+      ignore_watch : ["node_modules"],
+      max_memory_restart : "200M" ,
       env: {
         COMMON_VARIABLE: 'true'
       },
       env_production : {
         NODE_ENV: 'production'
       }
-    },
-
-    // Second application
-    // {
-    //   name      : 'WEB',
-    //   script    : 'web.js'
-    // }
+    }
   ],
-
   /**
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
@@ -36,6 +31,6 @@ module.exports = {
       repo : 'git@github.com:weijie9520/blog-koa2-api.git',
       path : '/home/blog/blog-koa2-api',
       'post-deploy' : 'yarn install && pm2 reload ecosystem.config.js --env production'
-    }
+    },
   }
 };
